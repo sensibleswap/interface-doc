ef05713f92ea0bd397128c37a53b05968a247c89
ccoincash <ccoincash@gmail.com>
2021-06-07 10:25:03 +0800

    update swap algo

--- swapAlgo.js
+++ swapAlgo.js
@@ -63,6 +63,23 @@ swapAlgo.swapToken1ToToken2 = function(token1AddAmount, swapToken1Amount, swapTo
   return [token2RemoveAmount, projFee]
 }
 
+// 交换token1到token2时，通过token2的数量计算token1的数量
+swapAlgo.swapToken1ToToken2ByToken2 = function(token2RemoveAmount, swapToken1Amount, swapToken2Amount, swapFeeRate, projFeeRate) {
+  token2RemoveAmount = BigInt(token2RemoveAmount)
+  swapToken1Amount = BigInt(swapToken1Amount)
+  swapToken2Amount = BigInt(swapToken2Amount)
+
+  const token1AddAmountWithFee = token2RemoveAmount * swapToken1Amount * BigInt(swapAlgo.FEE_FACTOR) / (swapToken2Amount - token2RemoveAmount)
+
+  const token1AddAmount = token1AddAmountWithFee / BigInt(swapAlgo.FEE_FACTOR - swapFeeRate)
+
+  let projFee = token1AddAmount * BigInt(projFeeRate) / BigInt(swapAlgo.FEE_FACTOR)
+  if (projFee < swapAlgo.MIN_TOKEN1_FEE) {
+    projFee = 0n
+  }
+  return [token1AddAmount, projFee]
+}
+
 // 交换token2到token1时可获取的token2数量
 swapAlgo.swapToken2ToToken1 = function(token2AddAmount, swapToken1Amount, swapToken2Amount, swapFeeRate, projFeeRate) {
   token2AddAmount = BigInt(token2AddAmount)
@@ -75,4 +92,20 @@ swapAlgo.swapToken2ToToken1 = function(token2AddAmount, swapToken1Amount, swapTo
     projFee = 0n
   }
   return [token1RemoveAmount, projFee]
+}
+
+// 交换token2到token1时, 通过token1的数量计算token2的数量
+swapAlgo.swapToken2ToToken1ByToken1 = function(token1RemoveAmount, swapToken1Amount, swapToken2Amount, swapFeeRate, projFeeRate) {
+  token1RemoveAmount = BigInt(token1RemoveAmount)
+  swapToken1Amount = BigInt(swapToken1Amount)
+  swapToken2Amount = BigInt(swapToken2Amount)
+
+  const token1AddAmountWithFee = token1RemoveAmount * swapToken2Amount * BigInt(swapAlgo.FEE_FACTOR) / (swapToken1Amount - token1RemoveAmount)
+  const token2AddAmount =  token1AddAmountWithFee / BigInt(swapAlgo.FEE_FACTOR - swapFeeRate)
+
+  let projFee = token2AddAmount * swapToken1Amount * BigInt(projFeeRate) / (swapToken2Amount * BigInt(swapAlgo.FEE_FACTOR) + token2AddAmountWithFee)
+  if (projFee < swapAlgo.MIN_TOKEN1_FEE) {
+    projFee = 0n
+  }
+  return [token2AddAmount, projFee]
 }
